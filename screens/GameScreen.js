cb2b6a10a3f54ba8d67ccd8b9dec96609b2f4a92
Patrick Barroso <patrick.plbarroso@gmail.com>
2025-06-04 16:47:31 -0300

    Add game logic and style start game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,3 +1,102 @@
-function GameScreen(){};
+import { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import Title from '../components/ui/title'
+import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
+import InstructionText from '../components/ui/instructionText';
+import Card from '../components/ui/Card'
+
+function generateRandomBetween(min, max, exclude){
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (rndNum === exclude){
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return rndNum;
+    }
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+function GameScreen({userNumber, onGameOver}){
+    const initialGuess = generateRandomBetween(1, 100, userNumber);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+    useEffect(() => {
+        if (currentGuess === userNumber){
+            onGameOver();
+        }
+    }, [currentGuess, userNumber, onGameOver]);
+
+    function nextGuessHandler(direction){
+        if (
+            (direction === 'lower' && currentGuess <= userNumber) || 
+            (direction === 'greater' && currentGuess > userNumber)
+        ){
+            Alert.alert("Don't lie!", 'You know that this is wrong...', [
+                {terxt: 'Sorry!', style: 'cancel'}
+            ]);
+            return;
+        }
+
+        if (direction === 'lower'){
+            maxBoundary = currentGuess;
+        } else {
+            minBoundary = currentGuess;
+        }
+        const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+        setCurrentGuess(newRndNumber);
+    }
+
+    return (<View style={styles.screen}>
+        <View>
+            <Title>Opponent's guess</Title>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card>
+                <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+                             <Ionicons name="remove" size={24} color="white"/>
+                        </PrimaryButton>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+                            <Ionicons name="add" size={24} color="white"/>
+                        </PrimaryButton>    
+                    </View>
+                </View>
+            </Card>
+        </View>
+    </View>)
+};
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 40,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#ddb52f',
+        textAlign: 'center',
+        borderWidth: 2,
+        borderColor: '#ddb52f',
+        padding: 12
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1
+    },
+    instructionText: {
+        marginBottom: 12
+    }
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,55 +1,78 @@
-import { TextInput, View, StyleSheet } from 'react-native'
+import { useState } from 'react';
+import { TextInput, View, StyleSheet, Alert, Text } from 'react-native'
 
-import PrimaryButton from '../components/PrimaryButton';
+import PrimaryButton from '../components/ui/PrimaryButton';
+import Title from '../components/ui/title';
+import Colors from '../constants/colors'
+import Card from '../components/ui/Card'
+import InstructionText from '../components/ui/instructionText';
 
-function StartGameScreen(){
+function StartGameScreen({onPickNumber}){
+    const [enteredNumber, setEnteredNumber] = useState('');
+
+    function numberInputHandler(enteredText){
+        setEnteredNumber(enteredText);
+    }
+
+    function resetInputHandler(){
+        setEnteredNumber('');
+    }
+
+    function confirmInputHandler(){
+        const chosenNumber = parseInt(enteredNumber);
+
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99){
+            Alert.alert('Invalid number!', 
+            'Number has to be a number between 1 and 99.',
+            [{ text: 'Okay', style:'destructive', onPress: resetInputHandler}]
+        );
+        return;
+        }
+        onPickNumber(chosenNumber);
+    }
+    
     return (
-        <View style={styles.inputContainer}>
-            <TextInput 
-                style={styles.numberInput} 
-                maxLength={2} 
-                keyboardType='number-pad' 
-                autoCapitalize="none" 
-                autoCorrect={false}
-            />
-            <View style={styles.buttonsContainer}>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButton>Reset</PrimaryButton>
+        <View style={styles.rootContainer}>
+            <Title>Guess My Number</Title>
+            <Card>
+                <InstructionText>Enter a Number</InstructionText>
+                <TextInput 
+                    style={styles.numberInput} 
+                    maxLength={2} 
+                    keyboardType='number-pad' 
+                    autoCapitalize="none" 
+                    autoCorrect={false}
+                    onChangeText={numberInputHandler}
+                    value={enteredNumber}
+                />
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                    </View>
                 </View>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButton>Confirm</PrimaryButton>
-                </View>
-            </View>
-            
+            </Card>
         </View>
-
     )
 }
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
+    rootContainer: {
+        flex: 1,
         marginTop: 100,
-        marginHorizontal: 24,
-        borderRadius: 8,
-        padding: 16,
-        backgroundColor: '#3b021f',
-        elevation: 4,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
-        alignItems: 'center',
-        justifyContent: 'center'
+        alignItems: 'center'
     },
     numberInput: {
         height: 50,
         width: 50,
         fontSize: 30,
-        borderBottomColor: '#ddd52f',
+        borderBottomColor: Colors.accent500,
         borderBottomWidth: 2,
-        color: '#ddd52f',
+        color: Colors.accent500,
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
